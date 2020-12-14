d12a1e43e0cfc8869e9d73461100ac4b3843c4f4
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2020-12-14 11:15:34 +0300

    Updates

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -21,6 +21,7 @@ from accounts import views as accounts_views
 from boards import views
 
 urlpatterns = [
+    path('Contact/', Contact_views.Contact, name='Contact'),
     path('signup/', accounts_views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
